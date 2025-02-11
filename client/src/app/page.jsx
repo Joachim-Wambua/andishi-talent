b7083bf297fd4361b526e7fa7a059c406f75397b
Joachim-Wambua <kimwambua96@gmail.com>
2025-02-11 16:32:48 +0300

    bug fix

--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import Homepage from "./(home)/page";
+import Homepage from "./home/page";
 
 export default function Home() {
   const [year] = useState(() => new Date().getFullYear());
