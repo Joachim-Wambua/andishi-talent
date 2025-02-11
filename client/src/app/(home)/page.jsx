7d52de6e7f464251643a473d04f5030535305a0c
Joachim-Wambua <kimwambua96@gmail.com>
2025-02-11 16:18:58 +0300

    bug fix

--- client/src/app/(home)/page.jsx
+++ client/src/app/(home)/page.jsx
@@ -1,18 +1,35 @@
 "use client";
 
 import Navbar from "./Navbar";
-import HeroSection from "./HeroSection";
+import Hero from "./Hero";
 import MissionStatement from "./MissionStatement";
 import Footer from "./Footer";
+import CourseCarousel from "./CourseCarousel";
+import ValueProposition from "./ValueProposition";
 
-export default function Home() {
+
+export default function Homepage() {
   return (
     <div className="min-h-screen flex flex-col bg-white text-gray-900">
+      {/* Navigation Bar */}
       <Navbar />
+
+      {/* Main Content */}
       <main className="flex-grow">
-        <HeroSection />
+        {/* Hero Section */}
+        <Hero />
+
+        {/* Mission Statement */}
         <MissionStatement />
+
+        {/* Course Carousel */}
+        <CourseCarousel />
+
+        {/* Why Us */}
+        <ValueProposition />
       </main>
+
+      {/* Footer */}
       <Footer />
     </div>
   );
--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -1,38 +1,10 @@
 "use client";
 
 import { useState } from "react";
-import Navbar from "./(home)/Navbar";
-import Hero from "./(home)/Hero";
-import MissionStatement from "./(home)/MissionStatement";
-import Footer from "./(home)/Footer";
-import CourseCarousel from "./(home)/CourseCarousel";
-import ValueProposition from "./(home)/ValueProposition";
+import Homepage from "./(home)/page";
 
 export default function Home() {
   const [year] = useState(() => new Date().getFullYear());
 
-  return (
-    <div className="min-h-screen flex flex-col bg-[#001014] text-white">
-      {/* Navigation Bar */}
-      <Navbar />
-
-      {/* Main Content */}
-      <main className="flex-grow">
-        {/* Hero Section */}
-        <Hero />
-
-        {/* Mission Statement */}
-        <MissionStatement />
-
-        {/* Course Carousel */}
-        <CourseCarousel />
-
-        {/* Why Us */}
-        <ValueProposition />
-      </main>
-
-      {/* Footer */}
-      <Footer />
-    </div>
-  );
+  return <Homepage />;
 }
